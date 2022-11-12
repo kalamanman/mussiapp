bc097ff826b2de5153151550fb55d5bb0ddfa9ec
unknown <newmankal@live.com>
2022-11-12 18:05:03 -0500

    sidebar

---
+++ mussiapp/src/components/sidebar/Sidebar.jsx
@@ -0,0 +1,36 @@
+import './Sidebar.css'
+import DashboardIcon from '../../assets/dashboard_icon.svg'
+import AddIcon from '../../assets/add_icon.svg'
+
+const Sidebar = () => {
+  return (
+    <div className='sidebar' >
+        <div className="content">
+            <div className="user">
+                {/*  avetar and user later */}
+                <p> hey user</p>
+            </div>
+            <nav className="links">
+                <ul>
+                    <li>
+                        <NavLink to ='/'>
+                            <img src={DashboardIcon} alt="dashboard icon"  />
+                            <span>Dashboard</span>
+                        </NavLink>
+                    </li>
+                    <li>
+                        <NavLink to ='/create'>
+                            <img src={AdddIcon} alt="add icon"  />
+                            <span>Add New Project</span>
+                        </NavLink>
+                    </li>
+                </ul>
+            </nav>
+        </div>
+        
+        Sidebar
+  </div>
+  )
+}
+
+export default Sidebar
