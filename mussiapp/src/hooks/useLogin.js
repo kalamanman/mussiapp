9348e2f65099822a773ab6039f730f0290aea042
unknown <newmankal@live.com>
2022-11-12 15:28:09 -0500

    added useLogin and useSignup

---
+++ mussiapp/src/hooks/useLogin.js
@@ -0,0 +1,29 @@
+import {useState} from 'react'
+import{useAutContext} from './useAuthContext'
+import {appAuth} from '../Firebase/config'
+
+export const useLogin =()=>{
+   const[status,setStatus] =useState(null)
+   const[error,setError] =useState(null)
+   const {user,dispatch} =useAutContext()
+const signIn=async(email,password)=>{
+    setError(null)
+    setStatus('loading')
+    try{
+    const res =  await 
+    appAuth.signInUserwithEmailAndPassword(email,password)
+    if(!res.user){
+        throw new Error('Could not sign you in !!')
+    }
+    console.log('login function',res.user)
+    setStatus('success')
+    dispatch({type:'LOGIN',payload:res.user})
+    }catch(err){
+        setError(err.message)
+        setStatus('Error')
+    }
+}
+
+return {status,error}
+
+}
---
+++ mussiapp/src/hooks/useSignup.js
@@ -0,0 +1,29 @@
+import {useState} from 'react'
+import{useAutContext} from './useAuthContext'
+import {appAuth} from '../Firebase/config'
+
+export const useLogin =()=>{
+   const[status,setStatus] =useState(null)
+   const[error,setError] =useState(null)
+   const {user,dispatch} =useAutContext()
+const signUp=async(email,password,displayName)=>{
+    setError(null)
+    setStatus('loading')
+    try{
+    const res =  await 
+    appAuth.createUserWithEmailAndPassword(email,password,displayName)
+    if(!res.user){
+        throw new Error('Could not sign you up !!')
+    }
+    console.log('signup function',res.user)
+    setStatus('success')
+    dispatch({type:'LOGIN',payload:res.user})
+    }catch(err){
+        setError(err.message)
+        setStatus('Error')
+    }
+}
+
+return {status,error}
+
+}
