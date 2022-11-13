7bd92eeed3893cf399d57581a141239c0e106d94
unknown <newmankal@live.com>
2022-11-13 00:04:44 -0500

    signup and css

--- mussiapp/src/pages/signup/Signup.jsx
+++ mussiapp/src/pages/signup/Signup.jsx
@@ -1,9 +1,41 @@
-
+import {useState} from 'react'
 //styles
 import './Signup.css'
 const Signup = () => {
+
+    const[email,setEmail]=useState('')
+    const[password,setPassword] =useState('')
+    const[displayName,setDisplayName]=useState('')
+    
   return (
-    <div>Signup</div>
+    <form  className="signup-form">
+        <label>
+            <span>email:</span>
+            <input type="text"
+               
+               onChange={(e)=>setEmail(e.target.value)}
+               value={email}             
+            />
+        </label>
+        <label>
+            <span>Password:</span>
+            <input type="password"
+               required
+               onChange={(e)=>setPassword(e.target.value)}
+               value={password}             
+            />
+        </label>
+        <label>
+            <span>Display Name:</span>
+            <input type="text"
+               required
+               onChange={(e)=>setDisplayName(e.target.value)}
+               value={displayName}             
+            />
+        </label>
+        
+        <button className="btn">Signup</button>
+    </form>
   )
 }
 
