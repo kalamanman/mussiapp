e8ac65e4e95f9d65bfa4c33cbc61f9cc3e1482e2
unknown <newmankal@live.com>
2022-11-14 22:50:48 -0500

    Added conditional rendering to Navbar

--- mussiapp/src/components/navbar/Navbar.jsx
+++ mussiapp/src/components/navbar/Navbar.jsx
@@ -19,10 +19,12 @@ const Navbar = () => {
             <li><Link to ='/login'>Login</Link></li>
             <li><Link to ='/signup'>Signup</Link></li>
             </>)}
+            {user && (
             <li>
              {!isPending && <button className="btn"onClick={logout}>Logout</button>}
              {isPending && <button className="btn"  disabled>Login out ...</button>}
               </li>
+              )}
         </ul>
     </div>
   )
